6185c69732d24a278aca8aab7f2c0973b149ace3
Nickyecen <nicolasecendron@gmail.com>
2024-10-09 22:12:17 -0300

    Added JFugue

---
+++ src/main/Main.java
@@ -0,0 +1,10 @@
+package main;
+
+import org.jfugue.player.Player;
+
+public class Main {
+    public static void main(String[] args) {
+    	Player player = new Player();
+    	player.play("A B C D");
+    }
+}
--- src/module-info.java
+++ src/module-info.java
@@ -5,4 +5,6 @@
  * 
  */
 module TrabalhoTCP {
+	requires java.desktop;
+	requires jfugue;
 }
