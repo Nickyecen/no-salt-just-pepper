f4d8afcbba8e1acacb9e82291da877671b767139
jspast <140563347+jspast@users.noreply.github.com>
2024-11-04 22:43:30 -0300

    Initial UI draft

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,16 @@
 package main;
 
+import javax.swing.SwingUtilities;
+import ui.MainFrame;
+
 public class Main {
     public static void main(String[] args) {	
 
+        // Use SwingUtilities to ensure that the UI updates happen on the Event Dispatch Thread (EDT)
+        SwingUtilities.invokeLater(() -> {
+            MainFrame mainFrame = new MainFrame();
+            mainFrame.setVisible(true);
+        });
+    	
     }
 }
---
+++ src/ui/MainFrame.java
@@ -0,0 +1,92 @@
+package ui;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class MainFrame extends JFrame {
+    public MainFrame() {
+        setTitle("Gerador de Música por Texto");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(800, 600);
+
+        // Layout
+        setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        
+        // Text Area
+        c.gridx = 0;
+    	c.gridy = 0;
+    	c.gridwidth = 4;
+    	c.gridheight = 1;
+    	c.weighty = 1.0;
+    	c.weightx = 1;
+    	c.fill = GridBagConstraints.BOTH;
+    	c.insets = new Insets(10, 10, 10, 10);
+    	JTextArea textArea = new JTextArea();
+    	textArea.setLineWrap(true);
+    	textArea.setWrapStyleWord(true);
+    	JScrollPane scrollPane = new JScrollPane(textArea);
+    	add(scrollPane, c);
+    	
+    	// Play Button
+        ImageIcon icon = new ImageIcon(getClass().getResource("/res/play_icon.png"));
+        c.gridx = 0;
+    	c.gridy = 1;
+    	c.gridwidth = 1;
+    	c.gridheight = 3;
+    	c.weighty = 0.0;
+    	c.weightx = 1.0;
+    	c.insets = new Insets(0, 10, 10, 10);
+	    add(new JButton(icon), c);
+	    
+	    // Stop Button
+	    icon = new ImageIcon(getClass().getResource("/res/stop_icon.png"));
+	    c.gridx = 1;
+	    c.gridy = 1;
+	    c.insets = new Insets(0, 0, 10, 0);
+	    add(new JButton(icon), c);
+	    
+	    // BPM Spinner
+	    c.gridx = 2;
+	    c.gridy = 1;
+	    c.gridheight = 1;
+	    c.fill = GridBagConstraints.VERTICAL;
+	    c.anchor = GridBagConstraints.LINE_END;
+	    c.insets = new Insets(0, 10, 10, 5);
+	    add(new JLabel("BPM:"), c);
+	    c.gridx = 3;
+	    c.gridy = 1;
+	    c.fill = GridBagConstraints.BOTH;
+	    c.anchor = GridBagConstraints.LINE_START;
+	    c.insets = new Insets(0, 0, 10, 10);
+	    add(new JSpinner(), c);
+	    
+	    // Instrument information
+	    c.gridx = 2;
+	    c.gridy = 2;
+	    c.fill = GridBagConstraints.VERTICAL;
+	    c.anchor = GridBagConstraints.LINE_END;
+	    c.insets = new Insets(0, 10, 10, 5);
+	    add(new JLabel("Current Instrument:"), c);
+	    c.gridx = 3;
+	    c.gridy = 2;
+	    c.fill = GridBagConstraints.VERTICAL;
+	    c.anchor = GridBagConstraints.LINE_START;
+	    c.insets = new Insets(0, 0, 10, 10);
+	    add(new JLabel("Helicopter (126)"), c);
+	    
+	    // Note information
+	    c.gridx = 2;
+	    c.gridy = 3;
+	    c.fill = GridBagConstraints.VERTICAL;
+	    c.anchor = GridBagConstraints.LINE_END;
+	    c.insets = new Insets(0, 10, 10, 5);
+	    add(new JLabel("Current Note:"), c);
+	    c.gridx = 3;
+	    c.gridy = 3;
+	    c.fill = GridBagConstraints.VERTICAL;
+	    c.anchor = GridBagConstraints.LINE_START;
+	    c.insets = new Insets(0, 0, 10, 10);
+	    add(new JLabel("A4"), c);
+    }
+}
